062501b039363eab8fe93ee4fee8511985673818
brainit-consulting <dutoit.emile@gmail.com>
2026-03-23 12:50:37 -0400

    Exp 246: DEPTH=6 AR=48 T=420s (NEW BEST 0.550270)

--- prepare.py
+++ prepare.py
@@ -30,7 +30,7 @@ import torch
 # ---------------------------------------------------------------------------
 
 MAX_SEQ_LEN = 2048          # context length
-TIME_BUDGET = 400           # training time budget in seconds (5 minutes)
+TIME_BUDGET = 420           # training time budget in seconds (5 minutes)
 EVAL_TOKENS = 40 * 524288   # number of tokens for validation eval
 VOCAB_SIZE = 8192
 
